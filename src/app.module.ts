88d585bd7c0548a204b2cde9cda6294898cd3b43
yoyopokki <hangookhelly@gmail.com>
2023-05-14 16:55:49 +1000

    refactor: put db settings to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
+import { User } from './user';
 import { Message } from './message';
 import { TelegramBotModule } from './telegram-bot';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'postgres',
-      entities: [User, Message],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRESQL_HOST'),
+        port: +configService.get('POSTGRESQL_PORT'),
+        username: configService.get('POSTGRESQL_USERNAME'),
+        password: configService.get('POSTGRESQL_PASSWORD'),
+        database: configService.get('POSTGRESQL_DATABASE'),
+        entities: [User, Message],
+        synchronize: true,
+      }),
     }),
     TelegramBotModule,
   ],
