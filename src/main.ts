20b4a1b73e5c4e99e01b8bc48c5a99386e10c45d
yoyopokki <hangookhelly@gmail.com>
2023-05-20 01:02:11 +1000

    feat: run bot after start app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TelegramBotService } from './telegram-bot';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const telegramBotService = app.get(TelegramBotService);
+  await telegramBotService.start();
+
   await app.listen(3000);
 }
 bootstrap();
